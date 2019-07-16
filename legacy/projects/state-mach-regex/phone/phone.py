4696233322a5c5fd05e97c8f74cae23216c1d950
mark-marshall <mark.marshallgp@gmail.com>
2019-07-16 18:40:38 +0100

    phone: added phone regex for valid number check

--- legacy/projects/state-mach-regex/phone/phone.py
+++ legacy/projects/state-mach-regex/phone/phone.py
@@ -1,20 +1,26 @@
 import re # module for processing regular expressions https://docs.python.org/3/library/re.html
 
 # Initial prompt to user
-line = input("Enter a phone number to validate or 'exit' when done. ")
+# line = input("Enter a phone number to validate or 'exit' when done. ")
 
-# TODO Define your regex
+# Define your regex
 
+phone_re = r"([0-9]{3}[-)\s]?[-)\s]?[0-9]{3}[-\s(]?[-)\s]?[0-9]{4})"
 
-while line != "exit":
-    # TODO Find matches
+matches = re.findall(phone_re, line[0])
 
+print(matches)
+
+# while line != "exit":
+    # TODO Find matches 
+    # matches = re.findall(phone_re, line)
     
     # TODO If no match found, print that no number was found
-   
+    # if not match:
+    #     print("No number was found")
    
     
-    # TODO Else, break number up into area code, prefix, and suffic
+    # TODO Else, break number up into area code, prefix, and suffix
     
     
     # As a stretch goal, you can modify your regex to search for country codes
@@ -22,4 +28,4 @@ while line != "exit":
     
     
     # Done validating, read in a new line
-    line = input("Enter a phone number to validate or 'exit' when done. ")
+    # line = input("Enter a phone number to validate or 'exit' when done. ")
