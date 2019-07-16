5b453849a4f52073fd89f54e63f3bc94d9d32f79
mark-marshall <mark.marshallgp@gmail.com>
2019-07-16 18:47:25 +0100

    phone: ammended regex to create separate capture groups for each number set

--- legacy/projects/state-mach-regex/phone/phone.py
+++ legacy/projects/state-mach-regex/phone/phone.py
@@ -1,31 +1,22 @@
 import re # module for processing regular expressions https://docs.python.org/3/library/re.html
 
 # Initial prompt to user
-# line = input("Enter a phone number to validate or 'exit' when done. ")
+line = input("Enter a phone number to validate or 'exit' when done. ")
 
 # Define your regex
+phone_re = r"[-(\s]?([0-9]{3})[-)\s]?[-)\s]?([0-9]{3})[-\s(]?[-)\s]?([0-9]{4})"
 
-phone_re = r"([0-9]{3}[-)\s]?[-)\s]?[0-9]{3}[-\s(]?[-)\s]?[0-9]{4})"
-
-matches = re.findall(phone_re, line[0])
-
-print(matches)
-
-# while line != "exit":
-    # TODO Find matches 
-    # matches = re.findall(phone_re, line)
+while line != "exit":
+    # Find matches 
+    match = re.match(phone_re, line)
     
-    # TODO If no match found, print that no number was found
-    # if not match:
-    #     print("No number was found")
+    # If no match found, print that no number was found
+    if not match:
+        print("Invalid number")
    
     
     # TODO Else, break number up into area code, prefix, and suffix
     
     
-    # As a stretch goal, you can modify your regex to search for country codes
-    # too and print that out as well!
-    
-    
     # Done validating, read in a new line
-    # line = input("Enter a phone number to validate or 'exit' when done. ")
+    line = input("Enter a phone number to validate or 'exit' when done. ")
